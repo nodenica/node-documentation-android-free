089e8e21b0f4a8836c9197b155d6b368c5accd11
Paulo McNally <paulomcnally@gmail.com>
2015-12-09 05:40:59 -0600

    v5.2.0

--- app/src/main/java/com/mc/nad/MainActivity.java
+++ app/src/main/java/com/mc/nad/MainActivity.java
@@ -1,24 +1,17 @@
 package com.mc.nad;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
+import android.support.v4.content.ContextCompat;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.LinearLayout;
 
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 
 import java.net.MalformedURLException;
@@ -36,14 +29,7 @@ import nad.mc.com.nodejsdocumentationfree.R;
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
-    /**
-     * The view to show the ad.
-     */
-    private AdView adView;
-    private AdRequest adRequest;
-    private String deviceid;
-
-    private static Toolbar mToolbar;
+    private static Toolbar toolbar;
 
 
     /* Your ad unit id. Replace with your actual ad unit id. */
@@ -54,11 +40,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // toolbar
-        setUpToolbar();
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+        setToolbarColors(R.color.color_primary, R.color.color_primary_dark);
 
-        // admob
-        adMobInit();
-
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         // WebView Instance
         webView = (WebView) findViewById(R.id.webView);
@@ -71,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                getSupportActionBar().setTitle(view.getTitle());
+                toolbar.setTitle(view.getTitle());
 
                 URL urls;
                 try {
@@ -81,82 +71,26 @@ public class MainActivity extends AppCompatActivity {
                             urls.getPath().lastIndexOf("/") + 1);
                     if (fileName != "index.html") {
                         // show back on ActionBar
-                        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                        toolbar.setNavigationIcon(R.drawable.ic_action_navigation_arrow_back);
                     } else {
                         // hide back on ActionBar
-                        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+                        toolbar.setNavigationIcon(R.drawable.ic_action_navigation_arrow_back);
                     }
                 } catch (MalformedURLException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
-
             }
         });
-
-    }
-
-    private void setUpToolbar() {
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        if (mToolbar != null) {
-            setSupportActionBar(mToolbar);
-            // Hide back on ActionBar
-            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-        }
-        setToolbarColors(R.color.color_primary, R.color.color_primary_dark);
     }
 
     public void setToolbarColors(int primaryColor, int primaryDarkColor) {
-        mToolbar.setBackgroundColor(getResources().getColor(primaryColor));
+        toolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), primaryColor));
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             // set colorPrimaryDark
             getWindow().setStatusBarColor(getResources().getColor(primaryDarkColor));
             getWindow().setNavigationBarColor(getResources().getColor(primaryColor));
         }
-    }
-
-    private void adMobInit() {
-        // Create an ad.
-        adView = new AdView(this);
-        adView.setAdSize(AdSize.SMART_BANNER);
-        adView.setAdUnitId(getString(R.string.admob_key));
-
-        // Add the AdView to the view hierarchy. The view will have no size
-        // until the ad is loaded.
-        LinearLayout layout = (LinearLayout) findViewById(R.id.ad);
-        layout.removeAllViews();
-
-        if (isConnected()) {
-            layout.setVisibility(View.VISIBLE);
-            layout.addView(adView);
-
-            final TelephonyManager tm = (TelephonyManager) getBaseContext()
-                    .getSystemService(Context.TELEPHONY_SERVICE);
-
-            deviceid = tm.getDeviceId();
-
-            // Create an ad request. Check logcat output for the hashed device
-            // ID to
-            // get test ads on a physical device.
-            adRequest = new AdRequest.Builder()
-                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                    .addTestDevice(deviceid).build();
-
-            // Start loading the ad in the background.
-            adView.loadAd(adRequest);
-        } else {
-            layout.setVisibility(View.GONE);
-        }
-    }
-
-    private boolean isConnected() {
-        ConnectivityManager cm = (ConnectivityManager) getApplicationContext()
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        boolean isConnected = false;
-        if (activeNetwork != null)
-            isConnected = true;
-        return isConnected;
     }
 
     @Override
@@ -193,38 +127,6 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (adView != null) {
-            adView.resume();
-        }
-    }
-
-    @Override
-    public void onPause() {
-        if (adView != null) {
-            adView.pause();
-        }
-        super.onPause();
-    }
-
-    /**
-     * Called before the activity is destroyed.
-     */
-    @Override
-    public void onDestroy() {
-        if (adView != null) {
-            adView.destroy();
-        }
-        super.onDestroy();
-    }
-
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        adMobInit();
     }
 
     @Override
